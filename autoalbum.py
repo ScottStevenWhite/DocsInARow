d61794665e6d6ba2106a6cce43e6cab3e81ad749
Scott White <stolidwisdom@gmail.com>
2023-05-24 13:37:20 -0400

    Title: Enhance Environment Variable Management and Add Argument Descriptions
    Details:
    - Implemented python-dotenv for robust environment variable management, enabling the program to read environment variables from a .env file.
    - Adjusted the set_config() function to read environment variables using os.getenv() method.
    - Created an example.env file to provide users a template for their .env files.
    - Improved the command-line argument parsing by adding descriptive help messages to the arguments.

--- autoalbum.py
+++ autoalbum.py
@@ -11,19 +11,38 @@ from google.cloud import vision
 import logging
 import argparse
 import re
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def load_config():
     """Load configuration from JSON file"""
-    with open('config.json') as config_file:
-        return json.load(config_file)
+    try:
+        logging.info("Loading configuration from config.json")
+        with open('config.json') as config_file:
+            config = json.load(config_file)
+        logging.info("Successfully loaded configuration")
+        return config
+    except (FileNotFoundError, IOError) as e:
+        logging.error(f"Error reading config file: {e}")
+        return None
+    except json.JSONDecodeError as e:
+        logging.error(f"Error parsing config file: {e}")
+        return None
 
 def get_image_files(pictures_dir):
     """Get image files from the specified directory"""
-    img_files = []
-    for file in os.listdir(pictures_dir):
-        if file.startswith("img"):
-            img_files.append(os.path.join(pictures_dir, file))
-    return img_files
+    try:
+        logging.info(f"Getting image files from {pictures_dir}")
+        img_files = []
+        for file in os.listdir(pictures_dir):
+            if file.startswith("img"):
+                img_files.append(os.path.join(pictures_dir, file))
+        logging.info(f"Found {len(img_files)} image files")
+        return img_files
+    except FileNotFoundError as e:
+        logging.error(f"Error reading directory: {e}")
+        return []
 
 def add_text_to_metadata(image_path, text):
     """Add text to the image metadata"""
@@ -39,11 +58,17 @@ def add_text_to_metadata(image_path, text):
 
 def extract_text(image_path):
     """Extract text from image using Pytesseract"""
-    return pytesseract.image_to_string(Image.open(image_path))
+    try:
+        return pytesseract.image_to_string(Image.open(image_path))
+    except (FileNotFoundError, IOError) as e:
+        logging.error(f"Error reading image: {e}")
+        return ""
+    except pytesseract.TesseractError as e:
+        logging.error(f"Error extracting text: {e}")
+        return ""
 
 def detect_labels(image_path):
     """Detect labels in the image using Google Vision"""
-    client = vision.ImageAnnotatorClient()
     client = vision.ImageAnnotatorClient()
     with open(image_path, 'rb') as image_file:
         content = image_file.read()
@@ -125,55 +150,70 @@ def has_more_than_25_words(string):
     """Check if string has more than 25 words"""
     return len(string.split()) > 25
 
-def main(skip_prompt=False):
-    logging.basicConfig(level=logging.INFO)
+def set_config():
+    """Load configuration and set environment variables"""
+    load_dotenv()
     config = load_config()
+    if config is None:
+        return None
 
-    # Set environment variables and configuration parameters
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config["google_application_credentials"]
-    openai.api_key = config["openai_api_key"]
-    pytesseract.pytesseract.tesseract_cmd = config["tesseract_cmd"]
+    logging.info("Setting environment variables and configuration parameters")
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_CMD")
+    return config
+
+def process_image_file(img_file, api_key, pictures_dir):
+    """Process individual image file"""
+    logging.info(f"Processing {img_file}")
+    try:
+        text = extract_text(img_file)
+        if has_more_than_25_words(text):
+            corrected_text = correct_text(text, api_key)
+            print("------------------ Text after correction ------------------")
+            print(corrected_text)
+            add_text_to_metadata(img_file, corrected_text)
+            print("------------------ Category ------------------")
+            print(categorize_document(corrected_text, api_key))
+
+            # Generate filename and rename the file
+            new_filename = generate_filename(corrected_text, api_key)
+            os.rename(img_file, os.path.join(pictures_dir, new_filename))
+            print(f"Renamed {img_file} to {new_filename}")
+
+            # Move the file to the appropriate date directory in Documents
+            new_path = os.path.join(pictures_dir, new_filename)
+            move_file_to_date_dir(new_path)
+
+        else:
+            print("Google Vision")
+            labels = detect_labels(img_file)
+            print(labels)
+    except Exception as e:
+        logging.error(f"Error processing {img_file}: {e}")
+
+def main(skip_prompt=False):
+    logging.info("Starting program")
+    logging.basicConfig(level=logging.INFO)
+    config = set_config()
+    if config is None:
+        return
     pictures_dir = config["pictures_dir"]
-
     img_files = get_image_files(pictures_dir)
-
+    if not img_files:
+        return
     for img_file in img_files:
-        logging.info(f"Processing {img_file}")
-        try:
-            text = extract_text(img_file)
-            if has_more_than_25_words(text):
-                corrected_text = correct_text(text, openai.api_key)
-                print("------------------ Text after correction ------------------")
-                print(corrected_text)
-                add_text_to_metadata(img_file, corrected_text)
-                print("------------------ Category ------------------")
-                print(categorize_document(corrected_text, openai.api_key))
-
-                # Generate filename and rename the file
-                new_filename = generate_filename(corrected_text, openai.api_key)
-                os.rename(img_file, os.path.join(pictures_dir, new_filename))
-                print(f"Renamed {img_file} to {new_filename}")
-
-                # Move the file to the appropriate date directory in Documents
-                new_path = os.path.join(pictures_dir, new_filename)
-                move_file_to_date_dir(new_path)
-
-            else:
-                print("Google Vision")
-                labels = detect_labels(img_file)
-                print (labels)
-
-            # Ask user to continue or quit
-            if not skip_prompt:
-                user_input = input("Do you want to continue to the next image? (Y/N): ")
-                if user_input.lower() != 'y':
-                    break
-        except Exception as e:
-            logging.error(f"Error processing {img_file}: {e}")
+        process_image_file(img_file, config["openai_api_key"], pictures_dir)
+        # Ask user to continue or quit
+        if not skip_prompt:
+            user_input = input("Do you want to continue to the next image? (Y/N): ")
+            if user_input.lower() != 'y':
+                break
+    logging.info("Finished processing all images")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--skip_prompt", action='store_true', help="Skip the user prompt after each image")
+    parser = argparse.ArgumentParser(description='This is an image processing script that uses OpenAI and Google Vision to extract and correct text from images.')
+    parser.add_argument("--skip_prompt", action='store_true', help="If set, the script will not prompt the user after processing each image.")
     args = parser.parse_args()
     main(skip_prompt=args.skip_prompt)
