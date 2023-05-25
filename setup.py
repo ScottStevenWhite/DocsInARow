0ad6c3a874808a277c2bdcdf5a2ecbb9f5a26c85
Scott White <stolidwisdom@gmail.com>
2023-05-25 19:57:52 -0400

    Title: Enhance setup.py with more package metadata
    Body:
    This commit enriches the setup.py file with additional metadata to provide more context about the package. The following fields have been added:
    
    - description: A brief description of the project.
    - long_description: An extensive description loaded from the README file.
    - long_description_content_type: The format of the long description (Markdown in this case).
    - author & author_email: The author's name and contact email.
    - url: The URL of the project repository.
    - classifiers: Meta-data tags indicating project status, audience, license, and supported Python versions.
    
    These additions will make it easier for other developers to understand the purpose and status of the project, enhancing its usability and accessibility.

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="YourPackageName",
+    name="DocsInARow",
     version="0.1",
+    description="A script for processing images and extracting, correcting, and categorizing text from them",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author="Scott White",
+    author_email="stolidwisdom@gmail.com",
+    url="https://github.com/ScottStevenWhite/DocsInARow",
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     install_requires=[
         "openai",
         "Pillow",
         "pytesseract",
         "python-dotenv",
         "google-cloud-vision",
-        "piexif",
-        # add any other packages your project depends on
+        "piexif"
     ],
 )
